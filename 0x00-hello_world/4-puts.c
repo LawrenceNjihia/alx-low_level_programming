19a95b094b2e836923941f1f07c437e7d625bf02
LawrenceNjihia <lawrence.njihia@gmail.com>
2022-09-08 15:34:46 -0700

    4puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
-nclude <stdio.h>
+#include <stdio.h>
 
+int main(void)
+{
 
+	puts("Programming is like building a multilingual puzzle\n");
 
-int main(void) { puts("Programming is like building a multilingual puzzle\n"); return (0); }
+	return (0);
+
+}
 
 
