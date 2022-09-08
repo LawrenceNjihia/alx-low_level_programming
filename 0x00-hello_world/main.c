227e5dd4f54870ddfde9a005ba3004919b2405d4
LawrenceNjihia <lawrence.njihia@gmail.com>
2022-09-08 15:57:39 -0700

    4gcc

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
-int main(void)
+/**
+ * main - entrypoint
+ *
+ * Return: always 0
+ */
+int main (void)
 {
-
 	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
-
 }
-
 
