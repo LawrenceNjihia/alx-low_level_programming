3c50edd63ea098a84b72f6ae0ac6922822144e3b
LawrenceNjihia <lawrence.njihia@gmail.com>
2022-09-12 12:30:25 -0700

    new4puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,16 @@
 #include <stdio.h>
 
 /**
- *
- * main - Prints a statement "Programming is like building a multilingual puzzle"
- *
+ * main - Prints a statement "Programming is like building 
+ * 			a multilingual puzzle"
  * Return: Always 0.
- *
  */
 
 int main(void)
+
 {
 
 	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 
 }
-
-
