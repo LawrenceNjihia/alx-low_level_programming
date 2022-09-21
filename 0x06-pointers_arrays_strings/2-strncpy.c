5ef84c1fd5a5d2bd3cac5b39172685e88604a379
LawrenceNjihia <lawrence.njihia@gmail.com>
2022-09-21 13:08:11 -0700

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *_strncpy - function copies a string
+ *Works identically to the standard library function 'strncpy'
+ *@dest: storing the string copy
+ *src: source string
+ *n: max number of byte copied
+ *Return: returns string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
+}
